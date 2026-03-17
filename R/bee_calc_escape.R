e30b68de8ad6df8e19f4bb78137fc8f9190ca4d5
VicoDelannoy <valefvre@gmail.com>
2026-03-17 18:41:46 +0100

    again

--- R/bee_calc_escape.R
+++ R/bee_calc_escape.R
@@ -41,9 +41,7 @@
 #' 
 #' @return
 #'  A list with one dataframe per pixel. Please make sure to call element of the
-#' list by their names 'my_list["108"]' and not by their positions e.g. use
-#' my_list["108"] to call pixel 108 this avoid any confusion between position in
-#' the list and pixel number. 
+#' list by their names and not by their positions. 
 #'
 #' @examples
 #' # Load data for example: library(BioExtremeEvent)
